8b53bff2b86eb109c80af23d0b76ac51c678fa33
tejas-xd <tskakde@gmail.com>
2023-01-26 01:01:53 +0530

    added dependancies
    Signed-off-by: tejas-xd <tskakde@gmail.com>

---
+++ lib/data_handling/models/models.dart
@@ -0,0 +1,193 @@
+
+class Mixed {
+  bool? adult;
+  String? backdropPath;
+  int? id;
+  String? title;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  String? posterPath;
+  String? mediaType;
+  double? voteAverage;
+
+  Mixed({
+    this.adult,
+    this.backdropPath,
+    this.id,
+    this.title,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.posterPath,
+    this.mediaType,
+    this.voteAverage,
+  });
+
+  Mixed.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    id = json['id'];
+    title = json['title'];
+    if (title == null) {
+      title = json['name'];
+    }
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    if (title == null) {
+      originalTitle = json['original_name'];
+    }
+    overview = json['overview'];
+    posterPath = json['poster_path'];
+    mediaType = json['media_type'];
+    voteAverage = json['vote_average'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['adult'] = adult;
+    data['backdrop_path'] = this.backdropPath;
+    data['id'] = this.id;
+    data['title'] = this.title;
+    if (title == null) {
+      data['name'] = this.title;
+    }
+
+    data['original_language'] = this.originalLanguage;
+    data['original_title'] = this.originalTitle;
+    if (title == null) {
+      data['original_name'] = this.originalTitle;
+    }
+
+    data['overview'] = this.overview;
+    data['poster_path'] = this.posterPath;
+    data['media_type'] = this.mediaType;
+    data['vote_average'] = this.voteAverage;
+    return data;
+  }
+}
+
+class Movie {
+  bool? adult;
+  String? backdropPath;
+  int? id;
+  String? title;
+  String? originalLanguage;
+  String? originalTitle;
+  String? posterPath;
+
+  Movie({
+    this.adult,
+    this.backdropPath,
+    this.id,
+    this.title,
+    this.originalLanguage,
+    this.originalTitle,
+    this.posterPath,
+  });
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    id = json['id'];
+    title = json['title'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    posterPath = json['poster_path'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['adult'] = this.adult;
+    data['backdrop_path'] = this.backdropPath;
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['original_language'] = this.originalLanguage;
+    data['original_title'] = this.originalTitle;
+    data['poster_path'] = this.posterPath;
+    return data;
+  }
+}
+
+class TVshow {
+  bool? adult;
+  String? backdropPath;
+  int? id;
+  String? name;
+  String? originalLanguage;
+  String? originalName;
+  String? posterPath;
+
+  TVshow({
+    this.adult,
+    this.backdropPath,
+    this.id,
+    this.name,
+    this.originalLanguage,
+    this.originalName,
+    this.posterPath,
+  });
+
+  TVshow.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    id = json['id'];
+    name = json['name'];
+    originalLanguage = json['original_language'];
+    originalName = json['original_name'];
+    posterPath = json['poster_path'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['adult'] = this.adult;
+    data['backdrop_path'] = this.backdropPath;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['original_language'] = this.originalLanguage;
+    data['original_name'] = this.originalName;
+    data['poster_path'] = this.posterPath;
+    return data;
+  }
+}
+
+class XGenres {
+  int? id;
+  String? name;
+
+  XGenres({this.id, this.name});
+
+  XGenres.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    return data;
+  }
+}
+
+
+
+class TVimbd {
+  int? id;
+
+  TVimbd(
+      {
+        this.id,
+        });
+
+  TVimbd.fromJson(Map<String, dynamic> json) {
+
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    return data;
+  }
+}
