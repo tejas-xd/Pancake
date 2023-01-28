4e016605377ded851380e44c2a780befe183b5ec
tejas-xd <tskakde@gmail.com>
2023-01-28 15:49:37 +0530

    added firebase auth and other features
    Signed-off-by: tejas-xd <tskakde@gmail.com>

---
+++ lib/googlesignin.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInProvider extends ChangeNotifier {
+  final googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
+
+  Future googleLogin() async {
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser == null) return;
+    _user = googleUser;
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await FirebaseAuth.instance.signInWithCredential(credential);
+    notifyListeners();
+  }
+
+  Future  logout() async{
+    await googleSignIn.disconnect();
+    FirebaseAuth.instance.signOut();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pancake/homescreen.dart';
 import 'package:flutter/services.dart';
+import 'package:pancake/loginscreen.dart';
+import 'package:pancake/shared/customvalues.dart';
+import 'package:provider/provider.dart';
+
+import 'googlesignin.dart';
 
 //ver 2
-void main() {
+Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((value) => runApp(const MyApp()));
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Homescreen();
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            canvasColor: xcanvas,
+            textTheme: TextTheme(
+              bodyText2: GoogleFonts.poppins(),
+            )
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const Main(),
+      ),
+    );
   }
 }
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong"),
+            );
+          }
+          if (snapshot.hasData) {
+            return Homescreen();
+          } else {
+            return LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
+
+
